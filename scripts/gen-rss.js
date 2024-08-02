d506f94b75f87db55e2ba3abac365b5146e83629
BhumikaPathak2 <pathakbhumika123@gmail.com>
2024-08-01 20:52:49 -0400

    removed other links

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+   
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
@@ -22,12 +20,7 @@ async function generate() {
       const frontmatter = matter(content)
 
       allPosts.push({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author
+        
       })
     })
   )
